5f9218133020c704942a515d33ee382ebc0b6d34
ihchoi <ihchoi95@gmail.com>
2024-05-23 14:47:28 +0900

    [green] add ReadCommand - TestExecuteInvalidArgument

--- SSDProject/TestShell/ReadCommand.cpp
+++ SSDProject/TestShell/ReadCommand.cpp
@@ -7,7 +7,9 @@ class ReadCommand : public Command {
 public:
 	ReadCommand(Receiver* receiver) : Command(receiver) {}
 	void execute(std::vector<std::string> v) const override {
-		// TODO: need to implement
+		if (v.size() < 2) {
+			throw invalid_argument("Need two argument for read command");
+		}
 	}
 private:
 	const string cmd = "R";
