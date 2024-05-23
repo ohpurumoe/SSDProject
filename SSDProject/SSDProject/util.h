bc34b396a6b7bfbe16488dd2cbd01edfe612c252
ssjl06 <ssjl06@naver.com>
2024-05-23 09:33:03 +0900

    [red] add parse unit tests

---
+++ SSDProject/SSDProject/util.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <stdexcept>
+#include <string>
+enum class Command{
+	READ,
+	WRITE
+};
+
+Command parse(int argc, char** argv){
+	return Command::WRITE;
+}
--- SSDProject/SSDTest/test.cpp
+++ SSDProject/SSDTest/test.cpp
@@ -1,7 +1,69 @@
-﻿#include "pch.h"
+﻿#include <cstring>
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
 #include "../SSDProject/StorageDriver.cpp"
+#include "../SSDProject/util.h"
 
+class parseTestFixture: public ::testing::Test{
+protected:
+	void SetUp() override {
+		argv = new char*[4];
+		for(int i = 0; i < 4; ++i){
+			argv[i] = new char[50];
+			memset(argv[i], '\0', 50 * sizeof(char));
+		}
+	}
+	void TearDown() override {
+		for(int i = 0; i < 4; ++i){
+			delete[] argv[i];
+		}
+		delete[] argv;
+	}
 
-TEST(SSDTest, StorageDriverTest) {
-	EXPECT_EQ(1, 1);
+	char** argv;
+};
+
+TEST_F(parseTestFixture, parseFail0){
+	EXPECT_THROW(parse(0, argv), std::exception);
+}
+
+TEST_F(parseTestFixture, parseFail1){
+	strcpy(argv[0], "wrongCmd");
+	EXPECT_THROW(parse(1, argv), std::exception);
+}
+
+TEST_F(parseTestFixture, parseFail2){
+	strcpy(argv[0], "ssd");
+	strcpy(argv[1], "WrongArg");
+	EXPECT_THROW(parse(3, argv), std::exception);
+}
+
+TEST_F(parseTestFixture, parseReadFail){
+	strcpy(argv[0], "ssd");
+	strcpy(argv[1], "R");
+	strcpy(argv[2], "5");
+	strcpy(argv[3], "wrongArg");
+	EXPECT_THROW(parse(4, argv), std::exception);
+}
+
+TEST_F(parseTestFixture, parseWriteFail){
+	strcpy(argv[0], "ssd");
+	strcpy(argv[1], "W");
+	strcpy(argv[2], "5");
+	EXPECT_THROW(parse(3, argv), std::exception);
+}
+
+TEST_F(parseTestFixture, parseRead){
+	strcpy(argv[0], "ssd");
+	strcpy(argv[1], "R");
+	strcpy(argv[2], "5");
+	EXPECT_EQ(parse(3, argv), Command::READ);
+}
+
+TEST_F(parseTestFixture, parseWrite){
+	strcpy(argv[0], "ssd");
+	strcpy(argv[1], "W");
+	strcpy(argv[2], "5");
+	strcpy(argv[3], "0x12345678");
+	EXPECT_EQ(parse(4, argv), Command::WRITE);
 }
