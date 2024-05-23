0b722a590b003d816757dc985481423f0638c0d4
th50-kim <artoflunar@gmail.com>
2024-05-23 15:30:06 +0900

    [red] TestApp1, 2를 위한 콘솔 출력 확인용 Fixture 추가 및 TC 추가

--- SSDProject/TestShell/TestShellApplication.cpp
+++ SSDProject/TestShell/TestShellApplication.cpp
@@ -6,6 +6,7 @@
 #include <iterator>
 
 #include "TestApp1Command.cpp"
+#include "TestApp2Command.cpp"
 
 using namespace std;
 
@@ -32,7 +33,6 @@ public:
 				executeCommand(cmd, v);
 			else
 				cout << "invalid command, try again" << endl;
-
 		}
 		catch (std::exception e) {
 			cout << e.what() << endl;
@@ -71,11 +71,12 @@ private:
 		else if (cmd == "fullwrite") {
 			// return new FullWriteCommand(&receiver);
 		}
-
 		else if (cmd == "testapp1") {
 			return new TestApp1Command();
 		}
-    
+		else if (cmd == "testapp2") {
+			return new TestApp2Command();
+		}
 		return nullptr;
 	}
 
