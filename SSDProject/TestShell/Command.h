ceaa22a95a7d6f5274c1304ec76d964bde29a92f
ihchoi <ihchoi95@gmail.com>
2024-05-23 10:36:02 +0900

    [green]  add sw ssd invoke api to command class

--- SSDProject/TestShell/Command.h
+++ SSDProject/TestShell/Command.h
@@ -3,19 +3,28 @@
 #include <string>
 #include <vector>
 
+using namespace std;
+
 class Command {
 public:
-    virtual ~Command() {}
-    virtual void execute(std::vector<std::string> v) const = 0;
+	virtual ~Command() {}
+	virtual void execute(std::vector<std::string> v) const = 0;
+
+	int invoke(string cmd) {
+		string arg = ssdExe + " " + cmd;
+		return system(arg.c_str());
+	}
+private:
+	const string ssdExe = "..\\x64\\Debug\\SSD.exe";
 };
 
 class Receiver {
 public:
-    // TODO : need to check whether print log or not.
-    void write() const { std::cout << "Executing write command" << std::endl; }
-    void read() const { std::cout << "Executing read command" << std::endl; }
-    void exit() const { std::cout << "Executing exit command" << std::endl; }
-    void help() const { std::cout << "Executing help command" << std::endl; }
-    void fullwrite() const { std::cout << "Executing fullwrite command" << std::endl; }
-    void fullread() const { std::cout << "Executing fullread command" << std::endl; }
+	// TODO : need to check whether print log or not.
+	void write() const { std::cout << "Executing write command" << std::endl; }
+	void read() const { std::cout << "Executing read command" << std::endl; }
+	void exit() const { std::cout << "Executing exit command" << std::endl; }
+	void help() const { std::cout << "Executing help command" << std::endl; }
+	void fullwrite() const { std::cout << "Executing fullwrite command" << std::endl; }
+	void fullread() const { std::cout << "Executing fullread command" << std::endl; }
 };
