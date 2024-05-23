af3355c479d9202a5ecdd864f5568de4203d1f70
y1117 <sim861117@gmail.com>
2024-05-23 13:28:40 +0900

    add creator

--- SSDProject/TestShell/Command.h
+++ SSDProject/TestShell/Command.h
@@ -5,8 +5,22 @@
 
 using namespace std;
 
+class Receiver {
+public:
+    // TODO : need to check whether print log or not.
+    void write() const { std::cout << "Executing write command" << std::endl; }
+    void read() const { std::cout << "Executing read command" << std::endl; }
+    void exit() const { std::cout << "Executing exit command" << std::endl; }
+    void help() const { std::cout << "Executing help command" << std::endl; }
+    void fullwrite() const { std::cout << "Executing fullwrite command" << std::endl; }
+    void fullread() const { std::cout << "Executing fullread command" << std::endl; }
+
+    void mock() const { std::cout << "Executing mock command" << std::endl; }
+};
+
 class Command {
 public:
+    Command(Receiver* receiver = nullptr) : receiver(receiver) {}
     virtual ~Command() {}
     virtual void execute(std::vector<std::string> v) const = 0;
     int invoke(std::string cmd) const {
@@ -15,6 +29,7 @@ public:
     }
 
 private:
+    Receiver* receiver;
     const std::string ssdExe = "..\\x64\\Debug\\SSD.exe";
 };
 
@@ -30,17 +45,4 @@ public:
     void execute(std::vector<std::string> v) const override;
 private:
     const string cmd = "W";
-};
-
-class Receiver {
-public:
-    // TODO : need to check whether print log or not.
-    void write() const { std::cout << "Executing write command" << std::endl; }
-    void read() const { std::cout << "Executing read command" << std::endl; }
-    void exit() const { std::cout << "Executing exit command" << std::endl; }
-    void help() const { std::cout << "Executing help command" << std::endl; }
-    void fullwrite() const { std::cout << "Executing fullwrite command" << std::endl; }
-    void fullread() const { std::cout << "Executing fullread command" << std::endl; }
- 
-    void mock() const { std::cout << "Executing mock command" << std::endl; }
 };
