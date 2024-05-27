6744096a7a506356ce9e248aaa4823aff0228a07
ihwachoi <ihchoi95@gmail.com>
2024-05-27 13:23:23 +0900

    [green] add InputValideChecker for checking input parameter validation

--- SSDProject/TestShell/InputValidChecker.cpp
+++ SSDProject/TestShell/InputValidChecker.cpp
@@ -12,15 +12,53 @@ public:
 	static const int TYPE_CMD_LBA_VAL = 2;
 
 	bool check(std::vector<std::string> args, int type = TYPE_CMD) {
+		if (args.empty()) {
+			return false;
+		}
+
+		if (type == TYPE_CMD) {
+			return (args.size() == 1);
+		}
+
+		if (type == TYPE_CMD_LBA)
+			return (args.size() == 2) && isValidLBA(args[1]);
+
+		if (type == TYPE_CMD_LBA_VAL) {
+			return (args.size() == 3) && isValidLBA(args[1]) && isValidValue(args[2]);
+		}
+
 		return false;
 	}
 
 private:
 	bool isValidLBA(const string lba) {
-		return false;
+		int num = -1;
+
+		try {
+			num = stoi(lba);
+		}
+		catch (...) {
+			return false;
+		}
+
+		return num >= 0 && num <= 99;
 	}
 
 	bool isValidValue(const string value) {
-		return false;
+
+		if (value.substr(0, 2) != "0x") {
+			return false;
+		}
+
+		string hexValue = value.substr(2);
+		for (char c : hexValue) {
+			if (!((c >= 'A' && c <= 'F') || (c >= '0' && c <= '9'))) {
+				return false;
+			}
+		}
+
+		return true;
 	}
+
+
 };
