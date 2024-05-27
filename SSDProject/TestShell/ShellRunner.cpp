2b0ce05592a7ea561ea04f10ed862f60a82cd29c
th50-kim <artoflunar@gmail.com>
2024-05-27 14:30:21 +0900

    ShellRunner Class 추가 및 main 함수 연결

---
+++ SSDProject/TestShell/ShellRunner.cpp
@@ -0,0 +1,38 @@
+﻿#include "TestShellApplication.cpp"
+#include <fstream>
+
+class ShellRunner {
+public:
+	ShellRunner(TestShellApplication* app) :
+		app(app) {}
+
+	void run(string filename) {
+		ifstream file;
+
+		file.open(filename);
+		string read;
+		while (file.peek() != EOF) {
+			getline(file, read);
+			cout << "read : " << read << endl;
+
+			app->execute(read);
+		}
+	}
+
+private:
+	string getStringToCommand(const string &str) {
+		if (str == "FullWriteReadCompare" )
+			return "testapp1";
+		else if (str == "FullRead10AndCompare")
+			return "testapp2";
+		else if (str == "Write10AndCompare")
+			return "testapp3";
+		else if (str == "Loop_WriteAndReadCompare")
+			return "testapp4";
+
+		return "";
+	}
+private:
+
+	TestShellApplication * app;
+};
