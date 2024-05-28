30d1d53e94d015d11663a612cf15c8212181e14c
ihwachoi <ihchoi95@gmail.com>
2024-05-28 14:15:14 +0900

    [green]  add flush command

--- SSDProject/TestShell/FlushCommand.cpp
+++ SSDProject/TestShell/FlushCommand.cpp
@@ -9,7 +9,21 @@ class FlushCommand : public Command {
 public:
 	FlushCommand(IReceiver* receiver) : Command(receiver) {}
 	void execute(vector<string> v) override {
+		if (receiver == nullptr) {
+			throw invalid_argument("Need valid read receiver");
+		}
 
+		if (!checker.check(v, InputValidChecker::TYPE_CMD)) {
+			throw invalid_argument("Need valid argument");
+		}
+
+		string flushCmd;
+		string space = " ";
+
+		flushCmd.append(cmd);
+
+		int ret = invoke(flushCmd);
+		receiver->flush(ret);
 	}
 private:
 	const string cmd = "F";
