db0ca2ad91eeebc0736139b66370db1a54a681c3
Yunsu kim <iamyunsu@naver.com>
2024-05-28 16:43:05 +0900

    [refactor] refactoring optimizeCommand 1,2,3

--- SSDProject/SSDProject/StorageDriver.cpp
+++ SSDProject/SSDProject/StorageDriver.cpp
@@ -52,82 +52,17 @@ public:
 	}
 	string optimizeCommandBuffer() {
 		mapbuffer = commandBuffer->flush();
-		vector<_Buffer> temp;
+		vector<_Buffer> Commands;
 
-		while (!mapbuffer.empty()) {
-			temp.push_back(mapbuffer.front());
-			mapbuffer.pop();
-		}
+		CopyBufferToVector(Commands);
 
-		for (int i = 0; i < temp.size(); i++) {
-			for (int j = i + 1; j < temp.size(); j++) {
-				if (temp[i].op == 'W' && temp[j].op == 'W' && temp[i].addr == temp[j].addr) {
-					temp.erase(temp.begin() + i);
-				}
-			}
-		}
+		OptimizeOverwrite(Commands);
+		OptimizeEraseAfterWrite(Commands);
+		OptimizeEraseTwice(Commands);
 
-		int low, high;
-		for (int i = 0; i < temp.size(); i++) {
-			if (temp[i].op == 'E') {
-				low = temp[i].addr;
-				high = temp[i].addr + temp[i].size;
-				for (int j = 0; j < i && j < temp.size(); j++) {
-					if (temp[j].op == 'W' && low <= temp[j].addr && temp[j].addr < high) {
-						temp.erase(temp.begin() + j);
-						j--;
-					}
-				}
-			}
-		}
-
-		string ret;
-		for (int i = 0; i < temp.size(); i++) {
-			if (temp[i].op == 'W') {
-				commandBuffer->write(temp[i].addr, temp[i].data);
-				ret += "W " + to_string(temp[i].addr) + " " + temp[i].data + "\n";
-			}
-			else if (temp[i].op == 'E') {
-				commandBuffer->erase(temp[i].addr, temp[i].size);
-				ret += "E " + to_string(temp[i].addr) + " " + to_string(temp[i].size) + "\n";
-			}
-		}
-
-		return ret;
+		return ProcessOptimizedCommand(Commands);
 	}
-	string optimizeCommandBuffer_3() {
-		mapbuffer = commandBuffer->flush();
-		vector<_Buffer> temp;
 
-		while (!mapbuffer.empty()) {
-			temp.push_back(mapbuffer.front());
-			mapbuffer.pop();
-		}
-
-		for (int i = 0; i < temp.size() - 1; ++i) {
-			if (temp[i].op == 'E' && temp[i + 1].op == 'E' &&
-				temp[i].addr + temp[i].size == temp[i + 1].addr &&
-				temp[i].size <= 10 && temp[i + 1].size <= 10) {
-				// 두 개의 Erase 명령어를 합침
-				temp[i].size += temp[i + 1].size;
-				temp.erase(temp.begin() + i + 1);
-			}
-		}
-
-		string ret;
-		for (int i = 0; i < temp.size(); i++) {
-			if (temp[i].op == 'W') {
-				commandBuffer->write(temp[i].addr, temp[i].data);
-				ret += "W " + to_string(temp[i].addr) + " " + temp[i].data + "\n";
-			}
-			if (temp[i].op == 'E') {
-				commandBuffer->erase(temp[i].addr, temp[i].size);
-				ret += "E " + to_string(temp[i].addr) + " " + to_string(temp[i].size) + "\n";
-			}
-		}
-
-		return ret;
-	}
 private:
 	IStorage* ssd;
 	Buffer* commandBuffer;
@@ -155,5 +90,72 @@ private:
 		if (LBA < 0 || LBA > 99) {
 			throw StorageException("LBA는 0 ~ 99 사이의 값이어야 합니다. LBA : " + to_string(LBA));
 		}
+	}
+
+	void CopyBufferToVector(vector<_Buffer>& commands)
+	{
+		while (!mapbuffer.empty()) {
+			commands.push_back(mapbuffer.front());
+			mapbuffer.pop();
+		}
+	}
+
+	void OptimizeOverwrite(vector<_Buffer>& commands)
+	{
+		for (int i = 0; i < commands.size(); i++) {
+			for (int j = i + 1; j < commands.size(); j++) {
+				if (commands[i].op == 'W' && commands[j].op == 'W' && commands[i].addr == commands[j].addr) {
+					commands.erase(commands.begin() + i);
+				}
+			}
+		}
+	}
+
+	void OptimizeEraseAfterWrite(vector<_Buffer>& commands)
+	{
+		int low, high;
+		for (int i = 0; i < commands.size(); i++) {
+			if (commands[i].op != 'E') continue;
+
+			low = commands[i].addr;
+			high = commands[i].addr + commands[i].size;
+			for (int j = 0; j < i && j < commands.size(); j++) {
+				if (commands[j].op == 'W' && low <= commands[j].addr && commands[j].addr < high) {
+					commands.erase(commands.begin() + j);
+					j--;
+				}
+			}
+		}
+	}
+
+	void OptimizeEraseTwice(vector<_Buffer>& commands)
+	{
+		for (int i = 0; i < commands.size() - 1; ++i) {
+			if (commands[i].op == 'E' && commands[i + 1].op == 'E' &&
+				commands[i].addr + commands[i].size == commands[i + 1].addr &&
+				commands[i].size <= 10 && commands[i + 1].size <= 10) {
+				// 두 개의 Erase 명령어를 합침
+				commands[i].size += commands[i + 1].size;
+				commands.erase(commands.begin() + i + 1);
+			}
+		}
+	}
+
+	string ProcessOptimizedCommand(vector<_Buffer>& commands)
+	{
+		string ret;
+
+		for (int i = 0; i < commands.size(); i++) {
+			if (commands[i].op == 'W') {
+				commandBuffer->write(commands[i].addr, commands[i].data);
+				ret += "W " + to_string(commands[i].addr) + " " + commands[i].data + "\n";
+			}
+			else if (commands[i].op == 'E') {
+				commandBuffer->erase(commands[i].addr, commands[i].size);
+				ret += "E " + to_string(commands[i].addr) + " " + to_string(commands[i].size) + "\n";
+			}
+		}
+
+		return ret;
 	}
 };
